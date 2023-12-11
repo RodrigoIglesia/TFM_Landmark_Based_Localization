8ee83b022880511a5ee2233ca40e7c8103b6397c
RodrigoIglesia <rodrigo@LAPTOP-0J1AVEOB>
2023-12-11 23:30:34 +0100

    Script to analyze .tfrecords content

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,27 +1,235 @@
+import os
+import sys
+
+import tensorflow.compat.v1 as tf
+import math
+import numpy as np
+import itertools
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+tf.enable_eager_execution()
+
 from waymo_open_dataset.utils import range_image_utils
 from waymo_open_dataset.utils import transform_utils
 from waymo_open_dataset.utils import  frame_utils
 from waymo_open_dataset import dataset_pb2 as open_dataset
 
-from google.cloud import storage
-
-import numpy as np
-import pandas as pd
-
-storage_client = storage.Client()
-bucket_name = "waymo_open_dataset_v_2_0_0"
-
-bucket = storage_client.bucket(bucket_name)
-
-blob = bucket.get_blob('validation/camera_image/10203656353524179475_7625_000_7645_000.parquet')
-
-# Imprime el nombre del archivo
-print(blob.name)
-
-# Imprime el tamaño del archivo
-print(blob.size)
-
-# Descarga el archivo
-blob.download_to_filename('validation/camera_image/10203656353524179475_7625_000_7645_000.parquet')
+# Add project src root to python path
+current_script_directory = os.path.dirname(os.path.realpath(__file__))
+src_dir = os.path.abspath(os.path.join(current_script_directory, ".."))
+sys.path.append(src_dir)
 
 
+def show_camera_image(camera_image, camera_labels, layout, cmap=None):
+    """Show a camera image and the given camera labels."""
+
+    ax = plt.subplot(*layout)
+
+    # Draw the camera labels.
+    for camera_labels in frame.camera_labels:
+        # Ignore camera labels that do not correspond to this camera.
+        if (camera_labels.name != camera_image.name):
+            continue
+
+        # Iterate over the individual labels.
+        for label in camera_labels.labels:
+            # Draw the object bounding box.
+            ax.add_patch(patches.Rectangle(
+            xy=(label.box.center_x - 0.5 * label.box.length,
+                label.box.center_y - 0.5 * label.box.width),
+            width=label.box.length,
+            height=label.box.width,
+            linewidth=1,
+            edgecolor='red',
+            facecolor='none'))
+
+        # Show the camera image.
+        plt.imshow(tf.image.decode_jpeg(camera_image.image), cmap=cmap)
+        plt.title(open_dataset.CameraName.Name.Name(camera_image.name))
+        plt.grid(False)
+        plt.axis('off')
+
+def plot_range_image_helper(data, name, layout, vmin = 0, vmax=1, cmap='gray'):
+    """Plots range image.
+
+    Args:
+        data: range image data
+        name: the image title
+        layout: plt layout
+        vmin: minimum value of the passed data
+        vmax: maximum value of the passed data
+        cmap: color map
+    """
+    plt.subplot(*layout)
+    plt.imshow(data, cmap=cmap, vmin=vmin, vmax=vmax)
+    plt.title(name)
+    plt.grid(False)
+    plt.axis('off')
+
+def get_range_image(laser_name, return_index):
+    """Returns range image given a laser name and its return index."""
+    return range_images[laser_name][return_index]
+
+def show_range_image(range_image, layout_index_start = 1):
+    """Shows range image.
+
+    Args:
+        range_image: the range image data from a given lidar of type MatrixFloat.
+        layout_index_start: layout offset
+    """
+    range_image_tensor = tf.convert_to_tensor(range_image.data)
+    range_image_tensor = tf.reshape(range_image_tensor, range_image.shape.dims)
+    lidar_image_mask = tf.greater_equal(range_image_tensor, 0)
+    range_image_tensor = tf.where(lidar_image_mask, range_image_tensor,
+                                    tf.ones_like(range_image_tensor) * 1e10)
+    range_image_range = range_image_tensor[...,0] 
+    range_image_intensity = range_image_tensor[...,1]
+    range_image_elongation = range_image_tensor[...,2]
+    plot_range_image_helper(range_image_range.numpy(), 'range',
+                    [8, 1, layout_index_start], vmax=75, cmap='gray')
+    plot_range_image_helper(range_image_intensity.numpy(), 'intensity',
+                    [8, 1, layout_index_start + 1], vmax=1.5, cmap='gray')
+    plot_range_image_helper(range_image_elongation.numpy(), 'elongation',
+                    [8, 1, layout_index_start + 2], vmax=1.5, cmap='gray')
+    
+
+def rgba(r):
+  """Generates a color based on range.
+
+  Args:
+    r: the range value of a given point.
+  Returns:
+    The color for a given range
+  """
+  c = plt.get_cmap('jet')((r % 20.0) / 20.0)
+  c = list(c)
+  c[-1] = 0.5  # alpha
+  return c
+
+def plot_image(camera_image):
+  """Plot a cmaera image."""
+  plt.figure(figsize=(20, 12))
+  plt.imshow(tf.image.decode_jpeg(camera_image.image))
+  plt.grid("off")
+
+def plot_points_on_image(projected_points, camera_image, rgba_func, point_size=5.0):
+    """Plots points on a camera image.
+
+    Args:
+        projected_points: [N, 3] numpy array. The inner dims are
+        [camera_x, camera_y, range].
+        camera_image: jpeg encoded camera image.
+        rgba_func: a function that generates a color from a range value.
+        point_size: the point size.
+
+    """
+    plot_image(camera_image)
+
+    xs = []
+    ys = []
+    colors = []
+
+    for point in projected_points:
+        xs.append(point[0])  # width, col
+        ys.append(point[1])  # height, row
+        colors.append(rgba_func(point[2]))
+
+    plt.scatter(xs, ys, c=colors, s=point_size, edgecolors="none")
+
+
+if __name__ == "__main__":
+    dataset_path = os.path.join(current_script_directory, "waymo_samples")
+
+    # for file in os.listdir(dataset_path):
+    file = "individual_files_validation_segment-10203656353524179475_7625_000_7645_000_with_camera_labels.tfrecord"
+    # Read the frame in tf records format
+    dataset = tf.data.TFRecordDataset(os.path.join(dataset_path, file), compression_type='')
+    print(dataset)
+
+    # Parse each frame to convert tfrecords to range
+    for data in dataset:
+        frame = open_dataset.Frame()
+        frame.ParseFromString(bytearray(data.numpy()))
+        # break loop for only visualizing the first frame
+        break
+
+    (range_images, camera_projections, _, range_image_top_pose) = frame_utils.parse_range_image_and_camera_projection(frame)
+    # print(frame.context)
+
+    ##################################################################
+    ## Plot camera images and labels
+    ##################################################################
+    plt.figure(figsize=(25, 20))
+    # Plot frame images
+    for index, image in enumerate(frame.images):
+        show_camera_image(image, frame.camera_labels, [3, 3, index+1])
+    plt.show()
+
+    ##################################################################
+    ## Plot range images
+    ##################################################################
+    plt.figure(figsize=(64,20))
+    frame.lasers.sort(key=lambda laser: laser.name)
+    show_range_image(get_range_image(open_dataset.LaserName.TOP, 0), 1)
+    show_range_image(get_range_image(open_dataset.LaserName.TOP, 1), 4)
+    plt.show()
+
+    ##################################################################
+    ## Point cloud visualization
+    ##################################################################
+    points, cp_points = frame_utils.convert_range_image_to_point_cloud(
+        frame,
+        range_images,
+        camera_projections,
+        range_image_top_pose)
+    points_ri2, cp_points_ri2 = frame_utils.convert_range_image_to_point_cloud(
+        frame,
+        range_images,
+        camera_projections,
+        range_image_top_pose,
+        ri_index=1)
+
+    # 3d points in vehicle frame.
+    points_all = np.concatenate(points, axis=0)
+    points_all_ri2 = np.concatenate(points_ri2, axis=0)
+    # camera projection corresponding to each point.
+    cp_points_all = np.concatenate(cp_points, axis=0)
+    cp_points_all_ri2 = np.concatenate(cp_points_ri2, axis=0)
+
+    # Number of points in each lidar
+    # First lidar
+    print(points_all.shape)
+    print(cp_points_all.shape)
+    print(points_all[0:2])
+    for i in range(5):
+        print(points[i].shape)
+        print(cp_points[i].shape)
+
+    # Second lidar
+    print(points_all_ri2.shape)
+    print(cp_points_all_ri2.shape)
+    print(points_all_ri2[0:2])
+    for i in range(5):
+        print(points_ri2[i].shape)
+        print(cp_points_ri2[i].shape)
+
+    images = sorted(frame.images, key=lambda i:i.name)
+    cp_points_all_concat = np.concatenate([cp_points_all, points_all], axis=-1)
+    cp_points_all_concat_tensor = tf.constant(cp_points_all_concat)
+
+    # The distance between lidar points and vehicle frame origin.
+    points_all_tensor = tf.norm(points_all, axis=-1, keepdims=True)
+    cp_points_all_tensor = tf.constant(cp_points_all, dtype=tf.int32)
+
+    mask = tf.equal(cp_points_all_tensor[..., 0], images[0].name)
+
+    cp_points_all_tensor = tf.cast(tf.gather_nd(
+        cp_points_all_tensor, tf.where(mask)), dtype=tf.float32)
+    points_all_tensor = tf.gather_nd(points_all_tensor, tf.where(mask))
+
+    projected_points_all_from_raw_data = tf.concat(
+        [cp_points_all_tensor[..., 1:3], points_all_tensor], axis=-1).numpy()
+    
+    plot_points_on_image(projected_points_all_from_raw_data, images[0], rgba, point_size=5.0)
+    plt.show()

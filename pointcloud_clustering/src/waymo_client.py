a6318b6712d734c1e9385ef070cdc2bad0d7a34f
RodrigoIglesia <rodrigo@LAPTOP-0J1AVEOB>
2024-05-18 00:39:34 +0200

    clustering service working with client

--- pointcloud_clustering/src/waymo_client.py
+++ pointcloud_clustering/src/waymo_client.py
@@ -4,24 +4,21 @@
 """
 import os
 import sys
-
-import logging
 import numpy as np
 import pathlib
-
+import struct
+import ctypes
 import rospy
-import rosbag
 
+import sensor_msgs.point_cloud2 as pc2
 from sensor_msgs.msg import PointCloud2, PointField, Image
 from waymo_parser.msg import CameraProj
-from pointcloud_clustering.srv import clustering_srv, clustering_srvRequest, clustering_srvResponse
+from pointcloud_clustering.srv import clustering_srv, clustering_srvRequest, landmark_detection_srv, landmark_detection_srvRequest
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-
 import tensorflow.compat.v1 as tf
 if not tf.executing_eagerly():
     tf.compat.v1.enable_eager_execution()
-
 
 # Add project root to python path
 current_script_directory = os.path.dirname(os.path.realpath(__file__))
@@ -33,102 +30,158 @@ from waymo_utils.waymo_3d_parser import *
 
 class WaymoClient:
     def __init__(self, cams_order):
-        rospy.loginfo("Initializing client parameters")
-        # Vector of integers determining the index order of the cameras, this vector also selects the number of cameras to use
         self.cams_order = cams_order
+        self.pointcloud = np.zeros((0, 3))
+        self.cluster_labels = np.zeros((0, 3))
+        self.image = np.array([])
+        self.image_height = 0
+        self.image_width = 0
+        self.intrinsic_matrix = np.zeros((3, 3))
+        self.extrinsic_matrix = np.zeros((4, 4))
+        self.extrinsic_matrix_inv = np.zeros((4, 4))
+        self.pointcloud_processor = PointCloudProcessor()
+        self.camera_processor = CameraProcessor()
 
-        # Service clients
+        rospy.loginfo("Waiting for server processes...")
+        # Initialize pointcloud process
         rospy.wait_for_service('process_pointcloud')
         self.pointcloud_clustering_client = rospy.ServiceProxy('process_pointcloud', clustering_srv)
         rospy.loginfo("Clustering service is running")
 
-        # PointCloud2 message definition
-        self.pointcloud_msg = PointCloud2()
-        self.pointcloud_msg.fields = [
+        # Initialize landmark process
+        rospy.wait_for_service('landmark_detection')
+        self.pointcloud_clustering_client = rospy.ServiceProxy('landmark_detection', clustering_srv)
+        rospy.loginfo("Landmark service is running")
+
+    def cart2hom(self, pts_3d):
+        """ Input: nx3 points in Cartesian
+            Oupput: nx4 points in Homogeneous by pending 1
+        """
+        n = pts_3d.shape[0]
+        pts_3d_hom = np.hstack((pts_3d, np.ones((n, 1))))
+        return pts_3d_hom
+
+    def process_pointcloud(self, frame):
+        points, points_cp = self.pointcloud_processor.get_pointcloud(frame)
+        if points is not None:
+            self.pointcloud_processor.pointcloud_to_ros(points)
+            request = clustering_srvRequest()
+            request.pointcloud = self.pointcloud_processor.pointcloud_msg
+
+            try:
+                rospy.loginfo("Calling pointcloud clustering service...")
+                response = self.pointcloud_clustering_client(request)
+                clustered_pointcloud = response.clustered_pointcloud
+                rospy.loginfo("Pointcloud received")
+                self.pointcloud_processor.respmsg_to_pointcloud(clustered_pointcloud)
+                rospy.loginfo("Pointcloud message decoded")
+            except rospy.ServiceException as e:
+                rospy.logerr(f"Service call failed: {e}")
+        else:
+            rospy.loginfo("No pointcloud in frame")
+
+    def process_image(self, frame):
+
+
+
+class PointCloudProcessor(WaymoClient):
+    def __init__(self):
+        self.pointcloud_msg = self.init_pointcloud_msg()
+
+    def init_pointcloud_msg(self):
+        pointcloud_msg = PointCloud2()
+        pointcloud_msg.fields = [
             PointField('x', 0, PointField.FLOAT32, 1),
             PointField('y', 4, PointField.FLOAT32, 1),
             PointField('z', 8, PointField.FLOAT32, 1),
             PointField('intensity', 12, PointField.FLOAT32, 1),
         ]
-        self.pointcloud_msg.is_bigendian = False
-        self.pointcloud_msg.point_step = 16  # Adjust point step based on number of fields
-        self.pointcloud_msg.height = 1
-        self.pointcloud_msg.is_dense = True
-        # Camera projection message
-        self.camera_params_msg = CameraProj()
-        # Image message definition
-        self.camera_msg = Image()
-        self.camera_msg.encoding = "bgr8"  # Set image encoding
-        self.camera_msg.is_bigendian = False
+        pointcloud_msg.is_bigendian = False
+        pointcloud_msg.point_step = 16
+        pointcloud_msg.height = 1
+        pointcloud_msg.is_dense = True
+        return pointcloud_msg
 
     def get_pointcloud(self, frame):
-        (range_images, camera_projections, segmentation_labels, range_image_top_pose) = frame_utils.parse_range_image_and_camera_projection(frame)
-        # Get points labeled for first and second return
-        # Parse range image for lidar 1
-        def _range_image_to_pcd(ri_index = 0):
-            points, points_cp = frame_utils.convert_range_image_to_point_cloud(
-                frame, range_images, camera_projections, range_image_top_pose,
-                ri_index=ri_index)
-            return points, points_cp
-        
-        # Return of the first 2 lidar scans
-        points_return1 = _range_image_to_pcd()
-        points_return2 = _range_image_to_pcd(1)
-
+        range_images, camera_projections, segmentation_labels, range_image_top_pose = frame_utils.parse_range_image_and_camera_projection(frame)
+        points_return1 = self._range_image_to_pcd(frame, range_images, camera_projections, range_image_top_pose)
+        points_return2 = self._range_image_to_pcd(frame, range_images, camera_projections, range_image_top_pose, ri_index=1)
         points, points_cp = concatenate_pcd_returns(points_return1, points_return2)
-
         return points, points_cp
     
+    def _range_image_to_pcd(self, frame, range_images, camera_projections, range_image_top_pose, ri_index=0):
+        points, points_cp = frame_utils.convert_range_image_to_point_cloud(
+            frame, range_images, camera_projections, range_image_top_pose, ri_index=ri_index)
+        return points, points_cp
+
     def pointcloud_to_ros(self, points):
         self.pointcloud_msg.width = points.shape[0]
         self.pointcloud_msg.row_step = self.pointcloud_msg.point_step * self.pointcloud_msg.width
-        
-        # Concatenate points and intensities if 'intensity' is provided
         data = np.column_stack((points, np.zeros((points.shape[0], 1), dtype=np.float32)))
         self.pointcloud_msg.data = data.tobytes()
 
-    def process_pointcloud(self, frame):
-        points, points_cp = self.get_pointcloud(frame)
-        if points is not None:
-            # Convert concatenated point cloud to ROS message
-            self.pointcloud_to_ros(points)
+    def respmsg_to_pointcloud(self, msg):
+        self.pointcloud = np.zeros((0, 3))
+        self.cluster_labels = np.zeros((0, 3))
+        gen = pc2.read_points(msg, skip_nans=True)
+        for x in gen:
+            test = x[3]
+            s = struct.pack('>f', test)
+            i = struct.unpack('>l', s)[0]
+            pack = ctypes.c_uint32(i).value
+            r = (pack & 0x00FF0000) >> 16
+            g = (pack & 0x0000FF00) >> 8
+            b = (pack & 0x000000FF)
+            self.pointcloud = np.append(self.pointcloud, [[x[0], x[1], x[2]]], axis=0)
+            self.cluster_labels = np.append(self.cluster_labels, [[r, g, b]], axis=0)
 
-            # Create a request object
-            request = clustering_srvRequest()
-            request.pointcloud = self.pointcloud_msg
+class CameraProcessor(WaymoClient):
+    def __init__(self):
+        self.camera_msg = self.init_camera_msg()
+        self.init_camera_params()
 
+    def init_camera_msg(self):
+        camera_msg = Image()
+        camera_msg.encoding = "bgr8"
+        camera_msg.is_bigendian = False
+        return camera_msg
 
-            # Call the clustering service
-            rospy.loginfo("Calling pointcloud clustering service...")
-            response = self.pointcloud_clustering_client(request)
-            clustered_pointcloud = response.clustered_pointcloud
+    def init_camera_params(self, frame):
+        camera_intrinsic = np.array(frame.context.camera_calibrations[0].intrinsic)
+        self.intrinsic_matrix = [[camera_intrinsic[0], 0, camera_intrinsic[2]],
+                            [0, camera_intrinsic[1], camera_intrinsic[3]],
+                            [0, 0, 1]]
+        distortion = np.array([camera_intrinsic[4], camera_intrinsic[5], camera_intrinsic[6], camera_intrinsic[7], camera_intrinsic[8]])
 
-            return clustered_pointcloud
-        else:
-            rospy.loginfo("No pointcloud in frame")
-            return None
+        self.extrinsic_matrix = np.array((frame.context.camera_calibrations[0].extrinsic.transform), dtype=np.float32).reshape(4, 4)
+        # Extract rotation and translation components from the extrinsic matrix
+        rotation = self.extrinsic_matrix[:3, :3]
+        translation = self.extrinsic_matrix[:3, 3]
+        # Invert the rotation matrix
+        rotation_inv = np.linalg.inv(rotation)
+        # Invert the translation
+        translation_inv = -np.dot(rotation_inv, translation)
+        # Construct the new extrinsic matrix (from camera to vehicle)
+        self.extrinsic_matrix_inv = np.zeros((4, 4), dtype=np.float32)
+        self.extrinsic_matrix_inv[:3, :3] = rotation_inv
+        self.extrinsic_matrix_inv[:3, 3] = translation_inv
+        self.extrinsic_matrix_inv[3, 3] = 1.0
+        self.extrinsic_matrix_inv = self.extrinsic_matrix_inv[:3,:]
 
     def get_camera_image(self, frame):
-        # Gets and orders camera images from left to right
-        cameras_images = {}
-        for i, image in enumerate(frame.images):
-            decoded_image = get_frame_image(image)
-            cameras_images[image.name] = decoded_image[...,::-1]
+        cameras_images = {image.name: get_frame_image(image)[..., ::-1] for image in frame.images}
         ordered_images = [cameras_images[1]]
-
         return ordered_images
 
     def process_camera(self, frame):
         camera_images = self.get_camera_image(frame)
         rospy.loginfo("Images loaded from dataset")
-        # Check whether 1 or more images have been selected
         if len(camera_images) == 1:
             image = camera_images[0]
-            self.camera_msg.height = image.shape[0]  # Set image height
-            self.camera_msg.width = image.shape[1]  # Set image width
+            self.camera_msg.height = image.shape[0]
+            self.camera_msg.width = image.shape[1]
             self.camera_msg.step = image.shape[1] * 3
             self.camera_msg.data = image.tobytes()
-
             return self.camera_msg
         elif len(camera_images) > 1:
             # TODO: Implement image stitching or process one by one.
@@ -137,13 +190,14 @@ class WaymoClient:
             rospy.loginfo("No image in frame")
             return None
 
-    def get_camera_params(self, frame):
-        camera_intrinsic = np.array(frame.context.camera_calibrations[0].intrinsic)
-        camera_extrinsic = np.array(frame.context.camera_calibrations[0].extrinsic.transform)
-        self.camera_params_msg.intrinsic = camera_intrinsic
-        self.camera_params_msg.extrinsic = camera_extrinsic
 
-        return self.camera_params_msg
+
+def plot_referenced_pointcloud(point_cloud):
+    import open3d as o3d
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(point_cloud)
+    mesh_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.6, origin=[0, 0, 0])
+    o3d.visualization.draw_geometries([pcd, mesh_frame])
 
 if __name__ == "__main__":
     rospy.init_node('waymo_client', anonymous=True)
@@ -151,39 +205,23 @@ if __name__ == "__main__":
 
     rate = rospy.Rate(1/30)  # Adjust the rate as needed
 
-    # TODO: Set dataset path as a configurable parameter
     dataset_path = os.path.join(src_dir, "dataset/waymo_map_scene")
     tfrecord_list = list(sorted(pathlib.Path(dataset_path).glob('*.tfrecord')))
 
-    wp = WaymoClient([2, 1, 3])
+    wc = WaymoClient([2, 1, 3])
 
-    ##############################################################
-    ## Iterate through Dataset Scenes
-    ##############################################################
-    for scene_index, scene_path in enumerate(sorted(tfrecord_list)):
-        scene_name = scene_path.stem
-        rospy.loginfo("Scene {}: {} processing: {}".format(str(scene_index), scene_name, scene_path))
+    while not rospy.is_shutdown():
+        for scene_index, scene_path in enumerate(tfrecord_list):
+            scene_name = scene_path.stem
+            rospy.loginfo(f"Scene {scene_index}: {scene_name} processing: {scene_path}")
+            frame = next(load_frame(scene_path))
+            if frame is not None:
+                rospy.loginfo("Calling processing services")
+                wc.pointcloud_processor.pointcloud_msg.header.frame_id = f"base_link_{scene_name}"
+                wc.pointcloud_processor.pointcloud_msg.header.stamp = rospy.Time.now()
+                wc.process_pointcloud(frame)
+                if wc.pointcloud_processor.pointcloud.size > 0:
+                    rospy.loginfo("Processed Pointcloud received")
+                    plot_referenced_pointcloud(wc.pointcloud_processor.pointcloud)
 
-        frame = next(load_frame(scene_path))
-        if frame is not None:
-            rospy.loginfo("Calling processing services")
-            # Process point cloud
-            wp.pointcloud_msg.header.frame_id = f"base_link_{scene_name}"
-            wp.pointcloud_msg.header.stamp = rospy.Time.now()
-            clustered_pointcloud = wp.process_pointcloud(frame)
-            rospy.loginfo("Processed Pointcloud received")
-
-
-            # # Process camera parameters
-            # wp.camera_params_msg.header.frame_id = f"base_link_{scene_name}"
-            # wp.camera_params_msg.header.stamp = rospy.Time.now()
-            # camera_params = wp.get_camera_params(frame)
-
-
-            # # Process camera image
-            # wp.camera_msg.header.frame_id = f"base_link_{scene_name}"
-            # wp.camera_msg.header.stamp = rospy.Time.now()
-            # camera_image = wp.process_camera(frame)
-  
-
-            rate.sleep()
+                rate.sleep()
